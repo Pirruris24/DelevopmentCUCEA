d69b59e8048d383d2b63dce08ae6bac1504013b4
User <israel.colima2892@alumnos.udg.mx>
2023-10-03 12:07:35 -0600

    Start Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
